a56e0ec783a56be4a1947f637e2104581bbb5f3e
Dmitrii <dmitrylor@gmail.com>
2025-04-18 10:27:41 +0100

    Add postgres and admin panel

--- backend/student_assistant/settings.py
+++ backend/student_assistant/settings.py
@@ -156,8 +156,12 @@ WSGI_APPLICATION = "student_assistant.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "student_assistant"),
+        "USER": os.environ.get("DB_USER", "postgres"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
